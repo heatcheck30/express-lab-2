6e58e2ff7d889f62de7cbb4e3431d357e52e2da4
Winston Tang <winstontang@Winstons-MacBook-Pro.local>
2022-10-17 17:47:18 -0700

    feat: Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const itr = [
+const port = 3000;
+const responses = [
   "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"
 ]
 
 app.get("/", (req, res) => {
-  res.send("<H1>Hey there!!!</H1>");
+  res.send("<H1>Hey there!!!</H1>"); //! browser says Hey there!
 });
 
 app.get("/greeting/:name?", (req, res) => {
@@ -23,38 +24,13 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 
-app.get('/magic', (req, res) => {
-  let htmlResponse = "";
-  for(let itr = 0; itr < arr.length; itr++) {
-      htmlResponse += "<h1>" + arr[itr] + "</h1>";
-  }
-
+app.get('/magic/:question', (req, res) => {
+  res.send(`<h1>${req.params.question} = ${responses[Math.floor(Math.random() *responses.length)]}</h1>`)
 
 })
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-});
+})
